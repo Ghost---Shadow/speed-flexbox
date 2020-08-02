d41d56099b25e007299d9a0c662fe12206d66477
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2020-08-02 10:07:10 +0530

    added canvas

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Canvas from './Canvas';
 
 const App = () => {
-  console.log();
+  const draw = (ctx) => {
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas draw={draw} />
     </div>
   );
 };
