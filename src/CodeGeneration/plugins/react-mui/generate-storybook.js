cc0e7f3ca8c5f305090c2a3db74056c42b007ff9
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2020-08-12 18:55:00 +0530

    Update story

--- src/CodeGeneration/plugins/react-mui/generate-storybook.js
+++ src/CodeGeneration/plugins/react-mui/generate-storybook.js
@@ -1,9 +1,23 @@
 const generate = () => {
   const code = `
   import React from 'react';
-import MyComponent from './MyComponent';
+  // import { action } from '@storybook/addon-actions';
+  import { withKnobs } from '@storybook/addon-knobs';
 
-export const MyComponent = () => <MyComponent />;
+  import MyComponent from './MyComponent';
+  
+  export default {
+    title: 'MyComponent',
+    component: MyComponent,
+    decorators: [withKnobs],
+  };
+  
+  export const normal = () => (
+    <MyComponent />
+  );
+
+  normal.story = { name: 'normal' };
+  
   `;
   return code;
 };
